f6c6e5a6750aa681263097f8a8823097b2d8c8c7
Emily <whynotbemily@gmail.com>
2026-06-04 20:21:28 -0400

    Create PASS project structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>PASS</h1>
+      <h2>Peer Academic Study Support</h2>
+
+      <p>
+        Share notes. Learn together. Succeed together.
+      </p>
+    </main>
+  );
+}
