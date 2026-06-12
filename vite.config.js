93a5763af8f5962c5ca7569af8449cc043550fea
Emily <whynotbemily@gmail.com>
2026-06-11 22:00:58 -0400

    trying to fix deployment bug attempt 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "peer-academic-study-support",
+  base: "/peer-academic-study-support/",
 });
